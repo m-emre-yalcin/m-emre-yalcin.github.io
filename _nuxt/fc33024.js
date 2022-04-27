a62be8bdde8e4a55a748643c6776207f2767e07b
m-emre-yalcin <m-emre-yalcin@users.noreply.github.com>
2022-04-27 11:31:09 +0000

    deploy: 1d33480739c12405cd3b2d32b816b9b73cae1e9f

--- _nuxt/fc33024.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{311:function(e,n,t){"use strict";t.r(n);var c=t(1).a.extend({}),r=t(63),component=Object(r.a)(c,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("cv")])}),[],!1,null,"b4e3d4a0",null);n.default=component.exports}}]);
