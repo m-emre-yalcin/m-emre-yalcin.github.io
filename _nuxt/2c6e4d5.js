1054ad09f42e2043ee18c73cae286f862598cdda
m-emre-yalcin <m-emre-yalcin@users.noreply.github.com>
2022-05-11 02:13:19 +0000

    deploy: 48296d23d264180a67dd2807c5791afee00f9417

--- _nuxt/2c6e4d5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[42],{331:function(n,e,t){"use strict";t.r(e);var o={mounted:function(){this.$router.push("/resume")}},d=t(9),component=Object(d.a)(o,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
